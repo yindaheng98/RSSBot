a6647b9890091e641ebab7abd6356b40ae169762
yindaheng98 <yindaheng98@163.com>
2022-03-28 20:41:58 +0800

    bug fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let envs = process.env;
 config = {
-    proxy: envs.HTTP_PROXY || envs.HTTPS_PROXY,
+    proxy: envs.GLOBAL_AGENT_HTTP_PROXY || envs.GLOBAL_AGENT_HTTPS_PROXY || envs.HTTP_PROXY || envs.HTTPS_PROXY,
     radar_url: envs.RADAR_URL || "https://raw.githubusercontent.com/DIYgod/RSSHub/master/assets/radar-rules.js",
     radar_update_interval: parseInt(envs.RADAR_UPDATE_INTERVAL) * 1000 || 3600,
     telegram_bot_token: envs.TELEGRAM_BOT_TOKEN,
